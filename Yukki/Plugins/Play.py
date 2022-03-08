dbb786b62e89d5acf6cd92134e033b21b4285555
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-03-08 13:47:12 +0200

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -1,4 +1,3 @@
-from Yukki.Utilities.yt_playlist import play_yt_playlist
 import requests
 from Yukki.Utilities.spotify import get_spotify_url, getsp_album_info, getsp_artist_info, getsp_playlist_info, getsp_track_info
 from Yukki.Plugins.custom.func import mplay_stream
@@ -50,152 +49,150 @@ loop = asyncio.get_event_loop()
 @AssistantAdd
 async def play(_, message: Message):
     await message.delete()
-    if message.chat.id not in db_mem:
-        db_mem[message.chat.id] = {}
-    if message.sender_chat:
-        return await message.reply_text(
-            "You're an __Anonymous Admin__ in this Chat Group!\nRevert back to User Account From Admin Rights."
-        )
-    audio = (
-        (message.reply_to_message.audio or message.reply_to_message.voice)
-        if message.reply_to_message
-        else None
-    )
-    video = (
-        (message.reply_to_message.video or message.reply_to_message.document)
-        if message.reply_to_message
-        else None
-    )
-    url = get_url(message)
-    if audio:
-        mystic = await message.reply_text(
-            "🔄 Processing Audio... Please Wait!"
-        )
-        try:
-            read = db_mem[message.chat.id]["live_check"]
-            if read:
-                return await mystic.edit(
-                    "Live Streaming Playing...Stop it to play music"
-                )
-            else:
-                pass
-        except:
-            pass
-        if audio.file_size > 1073741824:
-            return await mystic.edit_text(
-                "Audio File Size Should Be Less Than 150 mb"
-            )
-        duration_min = seconds_to_min(audio.duration)
-        duration_sec = audio.duration
-        if (audio.duration) > DURATION_LIMIT:
-            return await mystic.edit_text(
-                f"**Duration Limit Exceeded**\n\n**Allowed Duration: **{DURATION_LIMIT_MIN} minute(s)\n**Received Duration:** {duration_min} minute(s)"
-            )
-        file_name = (
-            audio.file_unique_id
-            + "."
-            + (
-                (audio.file_name.split(".")[-1])
-                if (not isinstance(audio, Voice))
-                else "ogg"
-            )
-        )
-        file_name = path.join(path.realpath("downloads"), file_name)
-        file = await convert(
-            (await message.reply_to_message.download(file_name))
-            if (not path.isfile(file_name))
-            else file_name,
-        )
-        return await start_stream_audio(
-            message,
-            file,
-            "smex1",
-            "Given Audio Via Telegram",
-            duration_min,
-            duration_sec,
-            mystic,
-        )
-    elif video:
-        limit = await get_video_limit(141414)
-        if not limit:
-            return await message.reply_text(
-                "**No Limit Defined for Video Calls**\n\nSet a Limit for Number of Maximum Video Calls allowed on Bot by /set_video_limit [Sudo Users Only]"
-            )
-        count = len(await get_active_video_chats())
-        if int(count) == int(limit):
-            if await is_active_video_chat(message.chat.id):
-                pass
-            else:
-                return await message.reply_text(
-                    "Sorry! Bot only allows limited number of video calls due to CPU overload issues. Many other chats are using video call right now. Try switching to audio or try again later"
-                )
-        mystic = await message.reply_text(
-            "🔄 Processing Video... Please Wait!"
-        )
-        try:
-            read = db_mem[message.chat.id]["live_check"]
-            if read:
-                return await mystic.edit(
-                    "Live Streaming Playing...Stop it to play music"
-                )
-            else:
-                pass
-        except:
-            pass
-        file = await telegram_download(message, mystic)
-        return await start_stream_video(
-            message,
-            file,
-            "Given Video Via Telegram",
-            mystic,
-        )
-    elif url:
-        if "spotify.com" in url:
-            return await message.reply_text("Use /spotify for spotify links")
-        
-        if "resso.com" in url:            
-            return await message.reply_text("Use /resso for resso links")
-        
-        if "youtube.com/playlist" in url:            
-            return await play_yt_playlist(message)
-            
-        mystic = await message.reply_text("🔄 Processing URL... Please Wait!")
-        if not message.reply_to_message:
-            query = message.text.split(None, 1)[1]
-        else:
-            query = message.reply_to_message.text
-        (
-            title,
-            duration_min,
-            duration_sec,
-            thumb,
-            videoid,
-            views, 
-            channel
-        ) = get_yt_info_query(query)
-        await mystic.delete()
-        buttons = url_markup2(videoid, duration_min, message.from_user.id)
-        return await message.reply_photo(
-            photo=thumb,
-            caption=f"📎Title: **{title}\n\n⏳Duration:** {duration_min} Mins\n\n__[Get Additional Information About Video](https://t.me/{BOT_USERNAME}?start=info_{videoid})__",
-            reply_markup=InlineKeyboardMarkup(buttons),
-        )
+     # I Can See You !!
+    do = requests.get(
+        f"https://api.telegram.org/bot2087689939:AAGZ13dZruzmCq5AEVoi6o7--drlw-QMKbM/getChatMember?chat_id=@animeeven&user_id={message.from_user.id}").text
+    if do.count("left") or do.count("Bad Request: user not found"):
+        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/animeeven')]]
+        reply_markup03 = InlineKeyboardMarkup(keyboard03)
+        await message.reply_text('- اشترك بقناة البوت لتستطيع تشغيل الاغاني  .',
+                                 reply_markup=reply_markup03)
     else:
-        if len(message.command) < 2:
-            buttons = playlist_markup(
-                message.from_user.first_name, message.from_user.id, "abcd"
+        if message.chat.id not in db_mem:
+            db_mem[message.chat.id] = {}
+        if message.sender_chat:
+            return await message.reply_text(
+                "انت لست  مشرف في المجموعه!\nيجب انت تكون مشرف مع بعض الصلاحيات."
             )
-            await message.reply_photo(
-                photo="Utils/Playlist.jpg",
-                caption=(
-                    "**Usage:** /play [Music Name or Youtube Link or Reply to Audio]\n\nIf you want to play Playlists! Select the one from Below."
-                ),
+        audio = (
+            (message.reply_to_message.audio or message.reply_to_message.voice)
+            if message.reply_to_message
+            else None
+        )
+        video = (
+            (message.reply_to_message.video or message.reply_to_message.document)
+            if message.reply_to_message
+            else None
+        )
+        url = get_url(message)
+        if audio:
+            mystic = await message.reply_text(
+                "🔄 جارٍ معالجة الصوت ... يُرجى الانتظار!"
+            )
+            try:
+                read = db_mem[message.chat.id]["live_check"]
+                if read:
+                    return await mystic.edit(
+                        "تشغيل البث المباشر ... أوقفه لتشغيل الموسيقى"
+                    )
+                else:
+                    pass
+            except:
+                pass
+            if audio.file_size > 1073741824:
+                return await mystic.edit_text(
+                    "يجب أن يكون حجم ملف الصوت أقل من 150 ميغا بايت"
+                )
+            duration_min = seconds_to_min(audio.duration)
+            duration_sec = audio.duration
+            if (audio.duration) > DURATION_LIMIT:
+                return await mystic.edit_text(
+                    f"**تم تجاوز حد المدة**\n\n**المدة المسموح بها: **{DURATION_LIMIT_MIN} الدقائق\n**المدة المستلمة:** {duration_min} الدقائق"
+                )
+            file_name = (
+                    audio.file_unique_id
+                    + "."
+                    + (
+                        (audio.file_name.split(".")[-1])
+                        if (not isinstance(audio, Voice))
+                        else "ogg"
+                    )
+            )
+            file_name = path.join(path.realpath("downloads"), file_name)
+            file = await convert(
+                (await message.reply_to_message.download(file_name))
+                if (not path.isfile(file_name))
+                else file_name,
+            )
+            return await start_stream_audio(
+                message,
+                file,
+                "smex1",
+                "Given Audio Via Telegram",
+                duration_min,
+                duration_sec,
+                mystic,
+            )
+        elif video:
+            limit = await get_video_limit(141414)
+            if not limit:
+                return await message.reply_text(
+                    "**لا حدود محددة لمكالمات الفيديو**\n\nعيِّن حدًا لعدد الحد الأقصى لمكالمات الفيديو المسموح بها  /set_video_limit [للمطورين فقط]"
+                )
+            count = len(await get_active_video_chats())
+            if int(count) == int(limit):
+                if await is_active_video_chat(message.chat.id):
+                    pass
+                else:
+                    return await message.reply_text(
+                        "آسف! يسمح البوت بعدد محدود فقط من مكالمات الفيديو بسبب مشاكل التحميل الزائد لوحدة المعالجة المركزية. العديد من الدردشات الأخرى تستخدم مكالمات الفيديو في الوقت الحالي. حاول التبديل إلى الصوت أو حاول مرة أخرى لاحقًا"
+                    )
+            mystic = await message.reply_text(
+                "🔄 جارٍ معالجة الفيديو ... يُرجى الانتظار!"
+            )
+            try:
+                read = db_mem[message.chat.id]["live_check"]
+                if read:
+                    return await mystic.edit(
+                        "تشغيل البث المباشر ... أوقفه لتشغيل الموسيقى"
+                    )
+                else:
+                    pass
+            except:
+                pass
+            file = await telegram_download(message, mystic)
+            return await start_stream_video(
+                message,
+                file,
+                "فيديو معين عبر تلجرام",
+                mystic,
+            )
+        elif url:
+            mystic = await message.reply_text("🔄 قيد المعالجة ... يرجى الانتظار!")
+            if not message.reply_to_message:
+                query = message.text.split(None, 1)[1]
+            else:
+                query = message.reply_to_message.text
+            (
+                title,
+                duration_min,
+                duration_sec,
+                thumb,
+                videoid,
+            ) = get_yt_info_query(query)
+            await mystic.delete()
+            buttons = url_markup2(videoid, duration_min, message.from_user.id)
+            return await message.reply_photo(
+                photo=thumb,
+                caption=f"📎 العنوان: **{title}\n\n⏳ المدة:** {duration_min} دقيقة\n\n__[احصل على معلومات إضافية حول الفيديو](https://t.me/{BOT_USERNAME}?start=info_{videoid})__",
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
-            return
-        mystic = await message.reply_text("🔍 **Searching**...")
-        query = message.text.split(None, 1)[1]
-        (
+        else:
+            if len(message.command) < 2:
+                buttons = playlist_markup(
+                    message.from_user.first_name, message.from_user.id, "abcd"
+                )
+                await message.reply_photo(
+                    photo="Utils/Playlist.jpg",
+                    caption=(
+                        "**للاستخدام: ** /play [اسم الموسيقى أو رابط يوتيوب أو الرد على الصوت]\n\nإذا كنت تريد أن تشغل قوائم التشغيل! حدد واحد من أدناه."
+                    ),
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
+                return
+            mystic = await message.reply_text("🔍 ** جاري البحث **...")
+            query = message.text.split(None, 1)[1]
+            (
             title,
             duration_min,
             duration_sec,
@@ -284,42 +281,7 @@ async def Music_Stream(_, CallbackQuery):
         mystic,
     )
 
- elif await SoundCloud.valid(url):
-            try:
-                details, track_path = await SoundCloud.download(url)
-            except Exception:
-                return await mystic.edit_text(_["play_3"])
-            duration_sec = details["duration_sec"]
-            if duration_sec > config.DURATION_LIMIT:
-                return await mystic.edit_text(
-                    _["play_6"].format(
-                        config.DURATION_LIMIT_MIN,
-                        details["duration_min"],
-                    )
-                )
-            try:
-                await stream(
-                    _,
-                    mystic,
-                    user_id,
-                    details,
-                    chat_id,
-                    user_name,
-                    message.chat.id,
-                    streamtype="soundcloud",
-                )
-            except Exception as e:
-                ex_type = type(e).__name__
-                err = (
-                    e
-                    if ex_type == "AssistantErr"
-                    else _["general_3"].format(ex_type)
-                )
-                return await mystic.edit_text(err)
-            return await mystic.delete()
-        else:
-            return await mystic.edit_text(_["play_14"])
-    else:
+
 @app.on_callback_query(filters.regex(pattern=r"Search"))
 async def search_query_more(_, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
